25c3d82f6f953d855ae086c8599d8cf111ee23b4
Rajaram Mohanty <rajaram.mohanty@wedopulse.co.in>
2023-04-06 13:39:29 +0530

    Auth init

---
+++ src/root.component.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { auth$, login } from "@demo/auth";
+import { Button } from "@demo/styleguide";
+import Loader from "./loader.component";
+
+
+export default function Root(props) {
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    let timeout;
+    const sub = auth$.subscribe(({ pending, error }) => {
+      // redirecting to /home when logged in will be done in the navbar. Cohesive code FTW!
+      setPending(pending);
+      setError(error);
+      timeout = setTimeout(() => {
+        setError();
+      }, 2000);
+    });
+    return () => {
+      clearInterval(timeout);
+      sub.unsubscribe();
+    };
+  }, []);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = document.forms.login.elements;
+    login(username.value, password.value);
+  };
+
+  return (
+    // <section>{props.name} is mounted!</section>
+  <div>
+      <form name="login" className="login-form" onSubmit={onSubmit}>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" required />
+      <label htmlFor="password">Password</label>
+      <input id="password" type="password" required />
+      <div>
+        <Button onClick={onSubmit}>{pending ? <Loader /> : "Log in"}</Button>
+      {/* <button type="submit" className="submit" disabled={pending}>
+          {pending ? <Loader /> : "Submit"}
+        </button> */}
+      </div>
+      {error && <div className="login-error">{error}</div>}
+    </form>
+  </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const { mergeWithRules } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+const mergeRulesByTestMatch = mergeWithRules({
+  module: {
+    rules: {
+      test: "match",
+      include: "replace",
+      exclude: "replace",
+      use: {
+        loader: "match",
+        options: "replace",
+      },
+    },
+  },
+});
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "demo",
+    projectName: "login",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return mergeRulesByTestMatch(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          include: undefined,
+          exclude: /\.lazy\.css$/i,
+        },
+        {
+          test: /\.lazy\.css$/i,
+          use: [
+            { loader: "style-loader", options: { injectType: "lazyStyleTag" } },
+            "css-loader",
+          ],
+        },
+      ],
+    },
+    devServer: {
+      port: 8502,
+      onListening: ({ compiler }) => {
+        const { https, client } = compiler.options.devServer;
+        const { publicPath, filename } = compiler.options.output;
+        const protocol = https ? "https://" : "http://";
+        const port = !client.port ? "" : `:${client.port}`;
+        const path = ["", "auto"].includes(publicPath) ? "/" : publicPath;
+        console.log(
+          `⚡️ single-spa application entry: ${protocol}${client.host}${port}${path}${filename}`
+        );
+      },
+    },
+  });
+
+};
